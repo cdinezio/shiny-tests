9c202f53efe5c41e9da9a9274c17ef0551f9919e
cdinezio <cdinezio@hotmail.com.ar>
2021-02-05 21:50:17 +0100

    Size changes possible and title.

--- user_selects_axis.R
+++ user_selects_axis.R
@@ -4,16 +4,23 @@ library(thematic)
 library(tidyverse)
 
 ui <- fluidPage(
-  fileInput("file", NULL, accept = c(".csv", ".tsv")),
-  numericInput("n", "Rows", value = 5, min = 1, step = 1),
-  textInput("ejex", "Write X axis name",value = "A"),
-  textInput("ejey", "Write Y axis name", value = "B"),
-  checkboxInput("color","Colour by some variable?"),
-  textInput("color_var", "If yes, which one?",value = "G"),
-  actionButton("graficar","Graficar los datos"),
-  tableOutput("head"),
-  verbatimTextOutput("datos"),
-  plotOutput("grafico",height = 1080,width = 1920)
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", NULL, accept = c(".csv", ".tsv")),
+      numericInput("n", "Rows", value = 5, min = 1, step = 1),
+      textInput("ejex", "Write X axis name",value = "A"),
+      textInput("ejey", "Write Y axis name", value = "B"),
+      checkboxInput("color","Colour by some variable?"),
+      textInput("color_var", "If yes, which one?",value = "G"),
+      actionButton("graficar","Graficar los datos"),
+      sliderInput("sizePoints","Size of points:",min = 1,max = 20,value = 3),
+      textInput("title","Plot title:"),
+      tableOutput("head"),
+      verbatimTextOutput("datos")),
+    mainPanel(
+      plotOutput("grafico")
+    )
+  )
 )
 
 server <- function(input, output, session) {
@@ -38,7 +45,8 @@ server <- function(input, output, session) {
   
   output$grafico <- renderPlot({
     req(input$graficar)
-    data() %>% ggplot(aes_string(input$ejex,input$ejey, colour = if_else(input$color,input$color_var,NULL))) + geom_point(size = 3)
+    data() %>% ggplot(aes_string(input$ejex,input$ejey, colour = if_else(input$color,input$color_var,NULL))) + 
+      geom_point(size = input$sizePoints) + labs(title = input$title)
   })
 }
 
